cce86554fb903735c6c320ee80029e416e4100bc
AndreGSabes <andresabes@outlook.com>
2020-10-22 22:05:09 -0300

    Removed not used icon.

--- src/components/PlaceToVisit.js
+++ src/components/PlaceToVisit.js
@@ -1,18 +1,17 @@
 import React from 'react';
-import {makeStyles} from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import ImageCard from './ImageCard';
 import places from '../static/places';
 import useWindowPosition from '../hook/useWindowPosition';
 import ExploreTwoToneIcon from '@material-ui/icons/ExploreTwoTone';
-import ExploreSharpIcon from '@material-ui/icons/ExploreSharp';
 
-const useStyles = makeStyles (theme => ({
+const useStyles = makeStyles((theme) => ({
   root: {
     minHeight: '100vh',
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
-    [theme.breakpoints.down ('md')]: {
+    [theme.breakpoints.down('md')]: {
       flexDirection: 'column',
     },
   },
@@ -22,9 +21,9 @@ const useStyles = makeStyles (theme => ({
   },
 }));
 
-export default function PlaceToVisit () {
-  const classes = useStyles ();
-  const checked = useWindowPosition ('header');
+export default function PlaceToVisit() {
+  const classes = useStyles();
+  const checked = useWindowPosition('header');
   return (
     <div className={classes.root} id="place-to-visit">
       <ImageCard place={places[1]} checked={checked} />
